76c7fd744e635d42da5478bdea046efe0595c1ee
jonathanjoelneptune <jonathanjoelneptune@gmail.com>
2026-09-15 13:02:46 -0700

    Store authoritative Week 1 territory ownership for Chronicle

--- chronicle-week-snapshots.js
+++ chronicle-week-snapshots.js
@@ -1,1 +1,14 @@
-window.OLYMPUS_CHRONICLE_SNAPSHOTS={1:{territoryCounts:{"Hercules Unchained":7,"The Peasants of Troy":7,"🏺Hades Nutz":7,"Winged Victory Formation":7,"Apollo's Archers":7,"BeginnersRuck":7,"Aries It Out":5,"Winging It":5,"TroyisBetterthanOdyssey":5,"The Beer of Olympus":5,"Flight of JarYES":5,"Uranus Colonizer":5}}};
+window.OLYMPUS_CHRONICLE_SNAPSHOTS={1:{territories:{
+"Hercules Unchained":["Hades' Gate","Eleusinian Sanctuary","Perseus' Watch","Pegasus' Spring","Thebes","Athena's Aegis Hall","Atlas' Mountains"],
+"The Peasants of Troy":["Calypso's Haven","Demeter's Harvest Fields","Gorgon Slayer's Landing","Pegasus Aerie","Threads of Destiny","Garden of the Hesperides","Hector's Ramparts"],
+"🏺Hades Nutz":["Rhodes","Athena's War Academy","Achilles' Training Grounds","Erebus","Circe's Enchanted Grove","Medusa's Grotto","Winged Messenger's Rest"],
+"Winged Victory Formation":["King Odysseus' Hall","Artemis' Moon Temple","Hector's Last Stand","River Styx","Heart of the Labyrinth","Palace of Hades","Hercules' Palaestra"],
+"Apollo's Archers":["Enchanted Court of Medea","Immortal's Citadel","Ares' Warroom","Theseus' Arena","Isle of Desire","Agamemnon's High Hall","Atlas' Mountains II"],
+"BeginnersRuck":["Troy","Zeus' Sky Temple","Fields of Ares","Odysseus' Harbor","Theseus' Hero's Path","Atlas' Pillars","Moirai Loom"],
+"Aries It Out":["Aphrodite's Garden","Huntress' Grove","Hector's Ramparts II","Calypso's Grotto","Athens"],
+"Winging It":["Temple of the Tides","Delphi","Forge of Hephaestus","Hall of the Twelve Labors","Jason's Hall"],
+"TroyisBetterthanOdyssey":["Hera's Sacred Grove","Agamemnon's War Council","Cronus' Fallen Keep","Apollo's Sun Court","Knossos"],
+"The Beer of Olympus":["Poseidon's Trident","Sparta","Hephaestus' Foundry","Argonauts' Landing","Prometheus' Crag"],
+"Flight of JarYES":["Hermes' Crossroads","Medea's Garden","Titan King's Throne","Petrified Garden","Queen's Sanctuary"],
+"Uranus Colonizer":["Tartarus","Apollo's Lyre Hall","Achilles' Last Stand","Elysium","Circe's Palace"]
+}}};
